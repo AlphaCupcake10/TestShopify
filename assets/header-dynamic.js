e5bd1ba5c7b259ebf9f34b92f48a3b6f012d95b0
AlphaCupcake10 <alphacupcake@outlook.com>
2023-09-05 17:26:41 +0530

    Fixed Navbar Color & hid cartDrawer for cart page

--- assets/header-dynamic.js
+++ assets/header-dynamic.js
@@ -1,45 +1,47 @@
-const header_dynamic_links = document.querySelector("#header-dynamic").querySelectorAll(".header-link");
-const header_dynamic_sections = document.querySelector("#header-dynamic").querySelectorAll(".expanded-menu-content");
+const header_dynamic_links = document
+    .querySelector("#header-dynamic")
+    .querySelectorAll(".header-link");
+const header_dynamic_sections = document
+    .querySelector("#header-dynamic")
+    .querySelectorAll(".expanded-menu-content");
 
 header_dynamic_sections[0].classList.add("active-menu");
 
-header_dynamic_links.forEach((Element,index)=>{
-    Element.addEventListener("mouseover",()=>{
-        header_dynamic_sections.forEach((item)=>{
+header_dynamic_links.forEach((Element, index) => {
+    Element.addEventListener("mouseover", () => {
+        header_dynamic_sections.forEach((item) => {
             item.classList.remove("active-menu");
-        })
-        header_dynamic_links.forEach((item)=>{
+        });
+        header_dynamic_links.forEach((item) => {
             item.classList.remove("active-menu");
-        })
+        });
         header_dynamic_sections[index].classList.add("active-menu");
         Element.classList.add("active-menu");
-    })
-})
+    });
+});
 
 let mobileNav = document.querySelector("#header-dynamic-mobile");
-function ToggleNavMenu()
-{
+function ToggleNavMenu() {
     mobileNav.classList.toggle("active-menu");
 }
 
-let submenus = document.querySelector("#header-dynamic-mobile").querySelectorAll(".sub-menu-title");
-submenus.forEach((Element)=>{
-    Element.addEventListener("click",()=>{
+let submenus = document
+    .querySelector("#header-dynamic-mobile")
+    .querySelectorAll(".sub-menu-title");
+submenus.forEach((Element) => {
+    Element.addEventListener("click", () => {
         Element.parentElement.classList.toggle("active-menu");
-    })
-})
+    });
+});
 
 let cartDrawerDynamic = document.querySelector("#cart-drawer-dynamic");
-function ToggleCartMenu()
-{
+function ToggleCartMenu() {
     cartDrawerDynamic.classList.toggle("cart-active");
 }
-function OpenCartMenu()
-{
+function OpenCartMenu() {
     cartDrawerDynamic.classList.add("cart-active");
 }
-function CloseCartMenu()
-{
+function CloseCartMenu() {
     cartDrawerDynamic.classList.remove("cart-active");
 }
 
@@ -53,16 +55,17 @@ function CloseCartMenu()
 //     }
 // })
 
-document.querySelector("#cart-drawer-dynamic").addEventListener("click",(e)=>{
-    CloseCartMenu();
-})
-document.querySelector(".cart-drawer-card").addEventListener("click",(e)=>{
+document
+    .querySelector("#cart-drawer-dynamic")
+    .addEventListener("click", (e) => {
+        CloseCartMenu();
+    });
+document.querySelector(".cart-drawer-card").addEventListener("click", (e) => {
     e.stopPropagation();
-})
-updateCartDrawer();
-async function updateCartDrawer()
-{
-    let response = await (await fetch('?/section_id=cart-drawer')).text();
+});
+updateCartDrawer(false);
+async function updateCartDrawer(openCart) {
+    let response = await (await fetch("?/section_id=cart-drawer")).text();
     console.log(response);
 
     const html = document.createElement("html");
@@ -74,45 +77,45 @@ async function updateCartDrawer()
     newBox = html.querySelector(".header-dynamic__cart-button").innerHTML;
     document.querySelector(".header-dynamic__cart-button").innerHTML = newBox;
 
-    const removeBtn = document.querySelector('.header-dynamic__remove-button');
-    removeBtn.addEventListener("click",()=>{
-        updateQuantity(removeBtn.getAttribute("data-key"),0);
-    })
+    const removeBtn = document.querySelector(".header-dynamic__remove-button");
+    removeBtn.addEventListener("click", () => {
+        updateQuantity(removeBtn.getAttribute("data-key"), 0);
+    });
 
-    document.querySelectorAll("#cart-drawer-dynamic .counter").forEach(counter=>{
-        const minus = counter.children[0];
-        const input = counter.children[1];
-        const plus = counter.children[2];
-        const key = counter.getAttribute("data-key");
-    
-        minus.addEventListener("click",()=>{
-            let current = parseInt(input.innerHTML,10);
-            current --;
-            input.innerHTML = current.toString();
-            updateQuantity(key,current);
-        })
-        plus.addEventListener("click",()=>{
-            let current = parseInt(input.innerHTML,10);
-            current ++;
-            input.innerHTML = current.toString();
-            updateQuantity(key,current);
-        })
-    })
+    document
+        .querySelectorAll("#cart-drawer-dynamic .counter")
+        .forEach((counter) => {
+            const minus = counter.children[0];
+            const input = counter.children[1];
+            const plus = counter.children[2];
+            const key = counter.getAttribute("data-key");
 
-    OpenCartMenu();
+            minus.addEventListener("click", () => {
+                let current = parseInt(input.innerHTML, 10);
+                current--;
+                input.innerHTML = current.toString();
+                updateQuantity(key, current);
+            });
+            plus.addEventListener("click", () => {
+                let current = parseInt(input.innerHTML, 10);
+                current++;
+                input.innerHTML = current.toString();
+                updateQuantity(key, current);
+            });
+        });
+
+    if (openCart) OpenCartMenu();
 }
 
-
-async function updateQuantity(key,quantity)
-{
-    const res = await fetch("/cart/update.js",{
-        method:"post",
-        headers:{
-            Accept:"application/json",
-            "Content-Type":"application/json"
+async function updateQuantity(key, quantity) {
+    const res = await fetch("/cart/update.js", {
+        method: "post",
+        headers: {
+            Accept: "application/json",
+            "Content-Type": "application/json",
         },
-        body:JSON.stringify({updates:{ [key]:quantity }})
-    })
+        body: JSON.stringify({ updates: { [key]: quantity } }),
+    });
     console.log(res);
-    updateCartDrawer();
+    updateCartDrawer(true);
 }
